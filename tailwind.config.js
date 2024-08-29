2713960d6284a1d0c7ff7b2ba9ed4a126aab2db4
Ayipz <muhammadarief1234516@gmial.com>
2024-08-29 19:43:39 +0700

    add: aos animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./resources/**/*.blade.php",
-    "./resources/**/*.jsx",
-    "./resources/**/*.js",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+    content: [
+        "./resources/**/*.blade.php",
+        "./resources/**/*.jsx",
+        "./resources/**/*.{ts,tsx,jsx}",
+    ],
+    theme: {
+        container: {
+            center: true,
+            padding: "2rem",
+            screens: {
+                "2xl": "1400px",
+            },
+        },
+        extend: {
+            aspectRatio: {
+                "4/3": "4 / 3",
+                "3/4": "3 / 4",
+            },
+        },
+    },
+    plugins: [],
+};
